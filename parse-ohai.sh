74ea206d4456bc83f36c89493a296da03e9f5a42
zac <z.t.lewis@lavabit.com>
2020-03-24 21:45:29 +1100

    Add script to extract ohai info.

---
+++ parse-ohai.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+OHAI="ohai_tmp.log"
+
+cp output.log $OHAI
+sed -i '/---BEGIN_OHAI_OUTPUT---/,$!d' $OHAI
+sed -i '/---END_OHAI_OUTPUT---/q' $OHAI
+sed -i 's/%!(PACKER_COMMA)/,/g' $OHAI
+sed '1,1d' $OHAI | sed '$d' | cut -d: -f2- > logs/ohai.log
+rm $OHAI
